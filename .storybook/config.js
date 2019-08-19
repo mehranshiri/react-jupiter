b8c652e4792409ed1cf2029ee1335edb3486ea8c
Arash Manteghi <arash.manteghi@gmail.com>
2019-08-19 12:34:07 +0430

    feat: Add storybook to project with simple example

---
+++ .storybook/config.js
@@ -0,0 +1,11 @@
+import { configure } from '@storybook/react';
+import requireContext from 'require-context.macro';
+
+const req = requireContext('../src', true, /\.stories.js$/);
+// const req = require.context('../src', true, /\.stories.js$/);
+
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
