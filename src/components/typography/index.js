f1d3e6cc357f8bdf3b62f29665e48f6b0c0acee0
Arash Manteghi <arash.manteghi@gmail.com>
2019-11-10 12:18:13 +0330

    fix: export default components of typography

--- src/components/typography/index.js
+++ src/components/typography/index.js
@@ -4,6 +4,12 @@ import Paragraph from './components/paragraph';
 import Title from './components/title';
 import Link from './components/link';
 
+export { default as Heading } from './components/heading';
+export { default as Text } from './components/text';
+export { default as Paragraph } from './components/paragraph';
+export { default as Title } from './components/title';
+export { default as Link } from './components/link';
+
 export default {
   Heading,
   Text,
