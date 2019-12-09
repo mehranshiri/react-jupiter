2a54a392fd54fde5edd29779eb2355ec1bdc90d7
marzzy <marzzy.m@gmail.com>
2019-12-09 15:57:48 +0330

    fix: change avatar sizes based on meetings doc

--- src/components/avatar/theme.js
+++ src/components/avatar/theme.js
@@ -3,9 +3,9 @@ import defaultTheme from '../themes';
 const avatarTheme = {
   borderRadius: defaultTheme.borderRadius.low,
   size: {
-    lg: 120,
-    md: 80,
-    sm: 40,
+    lg: 128,
+    md: 64,
+    sm: 32,
   },
 };
 
