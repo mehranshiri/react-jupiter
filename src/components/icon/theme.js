0f77e71d0f22f8ee42271ec25b7757e0d9b6a0ce
marzzy <marzzy.m@gmail.com>
2019-12-16 11:49:21 +0330

    feat(icon): add icon component

---
+++ src/components/icon/theme.js
@@ -0,0 +1,17 @@
+import defaultTheme from '../themes';
+
+const iconTheme = {
+  size: {
+    lg: 24,
+    md: 20,
+    sm: 16,
+  },
+  paddingSize: {
+    lg: 12,
+    md: 10,
+    sm: 8,
+  },
+  color: defaultTheme.colors,
+};
+
+export default iconTheme;
--- src/docs/containers/icon/index.js
+++ src/docs/containers/icon/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Typography } from '../../../components';
+import { Typography, Icon } from '../../../components';
 
 const { Heading } = Typography;
 
@@ -13,9 +13,37 @@ const IconPage = () => (
     <br />
 
     <Heading level={2}>
-      نمونه آیکون ساده
+      نمونه آیکون پیشفرض
     </Heading>
-    <p> hiiiiiiiiiiiiiii </p>
+    <Icon iconName="baseline-watch_later" />
+    <br />
+
+    <Heading level={2}>
+      نمونه آیکون با کامل ترین امکانات
+    </Heading>
+    <Icon iconName="baseline-watch_later" size="sm" color="green600" />
+    <br />
+
+    <Heading level={2}>
+      آیکون در اندازه های متفاوت
+    </Heading>
+
+    <Heading level={3}>
+      sm size
+    </Heading>
+    <Icon iconName="baseline-watch_later" size="sm" />
+    <br />
+
+    <Heading level={3}>
+      md size (default)
+    </Heading>
+    <Icon iconName="baseline-watch_later" size="md" />
+    <br />
+
+    <Heading level={3}>
+      lg size
+    </Heading>
+    <Icon iconName="baseline-watch_later" size="lg" />
     <br />
 
   </>
