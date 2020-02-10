c57ac53c22a4b75f51bf51f0951e8e7d15b05e34
marzzy <marzzy.m@gmail.com>
2020-02-10 14:23:27 +0330

    feat(lazy-cover): add lazy-loading feature to cover img

--- src/components/cover/index.style.js
+++ src/components/cover/index.style.js
@@ -1,11 +1,47 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import defaultImage from '../assets/defaults-images/Cover_default.jpg';
+import '../iranSharpFont.scss';
 
 export const Container = styled.div`
+  min-height: ${(props) => props.minHeight}px;
   & > a {
     display: block;
   }
+  
+  & > span {
+    display: block;
+    width: 100%;
+    min-height: inherit;
+  }
 `;
 
-export const Image = styled.img`
-  max-width: 100%;
+const lazyLoadingAnimation = keyframes`
+  100% {
+    opacity: 1;
+    filter: none;
+  }
+  0% {
+    opacity: 0;
+    filter: blur(20px);
+  }
+`;
+
+export const StyledLazyImage = styled(LazyLoadImage)`
+  min-height: inherit;
+  width: 100%;
+  font-family: 'IranSharp';
+  display: block;
+  background-image: url(${defaultImage});
+  background-size: cover;
+  animation: ${lazyLoadingAnimation} 2s 1;
+`;
+
+export const StyledImage = styled.img`
+  min-height: inherit;
+  width: 100%;
+  font-family: 'IranSharp';
+  display: block;
+  background-image: url(${defaultImage});
+  background-size: cover;
 `;
--- src/docs/containers/cover/index.js
+++ src/docs/containers/cover/index.js
@@ -18,28 +18,54 @@ const CoverPage = () => (
     </Heading>
     <Cover />
     <br />
+
     <Heading level={2}>
       کاور ساده با آدرس دهی اشتباه
     </Heading>
     <Cover src="wrong-src" />
     <br />
+
     <Heading level={2}>
       کاور ساده
     </Heading>
     <Cover src={sampleCover} />
     <br />
     <br />
+
     <Heading level={2}>
       کاور با لینک بیرونی
     </Heading>
     <Cover linkTo="http://yahoo.com" />
     <br />
+
     <Heading level={2}>
       کاور با لینک درونی
     </Heading>
     <Cover
       renderLink={(element) => <Link to="/components/card">{element}</Link>}
     />
+    <br />
+
+    <Heading level={2}>
+      کاور lazy-load
+    </Heading>
+    <Cover isLazy src="https://images.unsplash.com/photo-1563271963-9ff697463684?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" />
+    <br />
+    <Cover isLazy src="https://images.unsplash.com/photo-1568292342316-60aa3d36f4b3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1494&q=80" />
+    <br />
+    <Cover
+      isLazy
+      src="https://images.unsplash.com/photo-1566650141466-788ec481d69a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1055&q=80"
+      renderLink={(element) => <Link to="/components/card">{element}</Link>}
+    />
+    <br />
+    <Cover
+      isLazy
+      src="https://images.unsplash.com/photo-1543572414-f353d97a5212?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+      linkTo="http://yahoo.com"
+    />
+    <br />
+    <Cover isLazy src="wrong-lazy-src" minHeight={151} />
     <br />
 
   </>
