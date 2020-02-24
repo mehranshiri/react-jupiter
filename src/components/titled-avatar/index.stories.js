ac75b3b4188fa6158dca6b01ee96f7fad9f0e581
Arash Manteghi <arash.manteghi@gmail.com>
2020-02-24 14:14:46 +0330

    doc: add storybook for Button component

--- src/components/titled-avatar/index.stories.js
+++ src/components/titled-avatar/index.stories.js
@@ -1,46 +1,40 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-
 import TitledAvatar from './index';
+import '../storybook.css';
 
 storiesOf('TitledAvatar', module)
   .add(
     'small titled avatar',
     () => (
-      <div style={{ direction: 'rtl' }}>
-        <TitledAvatar
-          title="نام صاحب آواتار"
-          avatar="https://i.pravatar.cc/150?img=62"
-          avatarSize="sm"
-          titleSize={10}
-        />
-      </div>
+      <TitledAvatar
+        title="نام صاحب آواتار"
+        avatar="https://i.pravatar.cc/150?img=62"
+        avatarSize="sm"
+        titleSize={10}
+      />
     ),
   )
   .add(
     'medium titled avatar',
     () => (
-      <div style={{ direction: 'rtl' }}>
-        <TitledAvatar
-          title="نام صاحب آواتار"
-          avatar="https://i.pravatar.cc/150?img=62"
-          avatarSize="md"
-          titleSize={12}
-        />
-      </div>
+      <TitledAvatar
+        title="نام صاحب آواتار"
+        avatar="https://i.pravatar.cc/150?img=62"
+        avatarSize="md"
+        titleSize={12}
+      />
     ),
   )
   .add(
     'linked titled avatar',
     () => (
-      <div style={{ direction: 'rtl' }}>
-        <TitledAvatar
-          title="نام صاحب آواتار"
-          avatar="https://i.pravatar.cc/150?img=62"
-          avatarSize="md"
-          titleSize={12}
-          renderAvatarLink={(el) => (<a href="https://i.pravatar.cc/150?img=62">{el}</a>)}
-        />
-      </div>
+      <TitledAvatar
+        title="نام صاحب آواتار"
+        avatar="https://i.pravatar.cc/150?img=62"
+        avatarSize="md"
+        titleSize={12}
+        renderAvatarLink={(el) => (<a href="https://i.pravatar.cc/150?img=62">{el}</a>)}
+      />
     ),
   );
