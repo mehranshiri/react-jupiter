c2ceca7240410419f831169009f2a420bc40d67f
marzzy mogh <marzzy.m@gmail.com>
2020-04-19 13:37:58 +0430

    test: update snapshots and add story for form input

---
+++ src/components/form/text-input/index.stories.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+import {
+  withKnobs,
+  select,
+  text,
+  boolean,
+} from '@storybook/addon-knobs';
+import TextInput from './index';
+
+storiesOf('Form/Inputs', module)
+  .add('Minimal Text Input', () => <TextInput uniqueName="text-name-01" label="این لیبل است" />)
+  .add('Full option Text Input', () => (
+    <TextInput
+      uniqueName="text-name-02"
+      label={text('optional label', 'این لیبل است')}
+      placeholder={text('optional placeholder', 'متن پیشفرض')}
+      disabled={boolean('disabled(default : false)', false)}
+      description={text('optional description', 'توضیحات اضافه')}
+      errorMessage={text('optional err msg', '')}
+      type={select('select type(default : text)', ['text', 'password'])}
+    />
+  ))
+  .addDecorator(withInfo)
+  .addDecorator(withKnobs);
