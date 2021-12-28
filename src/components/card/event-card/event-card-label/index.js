0bb1cd0fa849dd9781e2197b2721dfc0489ff89a
mehranshiri <mehranshiri1996@gmail.com>
2021-12-28 11:12:57 +0330

    fix: add ads svg to event card

--- src/components/card/event-card/event-card-label/index.js
+++ src/components/card/event-card/event-card-label/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ImageLabel from './index.style';
 
-const adsLabelUrl = 'https://evand-storage.oss-eu-central-1.aliyuncs.com/assets/images/other/ads-label.svg';
+// const adsLabelUrl = 'https://evand-storage.oss-eu-central-1.aliyuncs.com/assets/images/other/ads-label.svg';
+const adsLabelUrl = require('../../../../docs/assets/ads-label.svg');
+
 const finishedLabelUrl = 'https://evand-storage.oss-eu-central-1.aliyuncs.com/assets/images/other/finished-text-label.svg';
 
 const EventCardLabel = ({ type }) => {
